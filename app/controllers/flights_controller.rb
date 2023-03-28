b87b6146780d3973fbcdff2b3020f9c6f1f21bf2
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 13:50:48 +0100

    Fix search form for flights
    - Had to tweak the form field names to get the params hash built like I
    want it
    - Update #index action to fetch available flights from the database
    through the Flights.search method
    - Update index view to display a list of the available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.codename, a.id] }
-    @date_options = Flight.all.map { |f| [f.date, f.id] }
+    @date_options = Flight.all.map { |f| [f.formatted_date, f.date] }
+
+    @available_flights = Flight.search(departure: params[:departure_airport_id],
+                                        arrival: params[:arrival_airport_id],
+                                        date_picked: params[:date].to_date)
   end
 end
