d1772fe49f62d21f506f35be7a321830202e1d3b
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 19:14:19 +0100

    Turn Flight.search into a scope
    - Renamed the method into 'available_flights' since it fits better
    with the concept of a scope

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -9,9 +9,9 @@ class FlightsController < ApplicationController
     @num_passengers = params[:num_passengers]
 
     if params[:commit].present?
-      @available_flights = Flight.search(departure: params[:departure_airport_id],
-                                         arrival: params[:arrival_airport_id],
-                                         date_picked: params[:date].to_date)
+      @available_flights = Flight.available_flights(departure: params[:departure_airport_id],
+                                                    arrival: params[:arrival_airport_id],
+                                                    date_picked: params[:date].to_date)
     end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,11 +3,11 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
-  def self.search(departure:, arrival:, date_picked:)
+  scope :available_flights, lambda { |departure:, arrival:, date_picked:|
     where(departure_airport_id: departure,
           arrival_airport_id: arrival,
           date: (date_picked.midnight..date_picked.midnight + 1.day))
-  end
+  }
 
   def formatted_date
     date.strftime("%d/%m/%Y")
