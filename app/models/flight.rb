6d4023e17c67ccbcfd22cf918b415d7dfe28c416
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 12:40:22 +0100

    Create Flight.search
    - This method queries the database for the flight that match the form
    params
    - Created Flight#formatted_date to have a cleaner display in the date
    dropdown

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,14 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.search(departure:, arrival:, date_picked:)
+    where(departure_airport_id: departure,
+          arrival_airport_id: arrival,
+          date: (date_picked.midnight..date_picked.midnight + 1.day))
+  end
+
+  def formatted_date
+    date.strftime("%m/%d/%Y")
+  end
 end
