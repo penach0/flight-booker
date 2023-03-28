f31b7e878c773fbe414682aab9a8cef51cf1915e
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 10:20:48 +0100

    Seed flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Flight.delete_all
 Airport.delete_all
 
 AIRPORT_CODES = ["LAX", "JFK", "CDG", "DXB", "SIN", "HKG", "LHR", "PVG", "ICN", "SYD"]
 
 AIRPORT_CODES.each { |code| Airport.create({ codename: code }) }
+
+10.times do |_i|
+  departure_airport_id = Airport.ids.sample
+  arrival_airport_id = (Airport.ids - [departure_airport_id]).sample
+
+  Flight.create!(departure_airport_id:,
+                 arrival_airport_id:,
+                 start: rand(Time.current..1.year.from_now),
+                 duration: 1.hour)
+end
