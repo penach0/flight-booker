ef05afbc2add62ce234c7510322b0a052cb76ac0
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 10:37:53 +0100

    Set up Flights routes
    - Set root to flights#index
    - Create index view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :flights
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "flights#index"
 end
