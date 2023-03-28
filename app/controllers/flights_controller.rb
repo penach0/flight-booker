5e819f8cf31cd4b7ef9d42c54ffed63da95ff333
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 11:37:43 +0100

    Finish flights#index form
    - Add dropdowns for number of passengers and date
    - Not sure if the form will work, it will probably need tweaks to the
    params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.codename, a.id] }
+    @date_options = Flight.all.map { |f| [f.date, f.id] }
   end
 end
