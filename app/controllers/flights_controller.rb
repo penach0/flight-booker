bcd713b90d2b4dba69c33704d6738096c4f2c8a0
penach0 <josemanuel.penacho@gmail.com>
2023-03-28 14:25:26 +0100

    Add check for params[:commit] in index action
    - Things were breaking because if the form is not submitted the
    available_flights can't be computed. This check ensures that a form as
    been submitted
    - Made it so that the date dropdown only shows unique dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,15 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.codename, a.id] }
-    @date_options = Flight.all.map { |f| [f.formatted_date, f.date] }
+    @date_options = Flight.all
+                          .order(:date)
+                          .map { |f| [f.formatted_date, f.date] }
+                          .uniq(&:first)
 
-    @available_flights = Flight.search(departure: params[:departure_airport_id],
-                                        arrival: params[:arrival_airport_id],
-                                        date_picked: params[:date].to_date)
+    if params[:commit].present?
+      @available_flights = Flight.search(departure: params[:departure_airport_id],
+                                         arrival: params[:arrival_airport_id],
+                                         date_picked: params[:date].to_date)
+    end
   end
 end
