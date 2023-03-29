552be133985cc10ff78df36dfa00f9e91610a271
penach0 <josemanuel.penacho@gmail.com>
2023-03-29 08:07:56 +0100

    Create #create action Bookings
    - Create strong params
    - Currently things are breaking because I am passing to many params.
    Some of the fields I included are just informative for the user and
    not actually important to be submitted (most can be fetch by the
    controller through the flight id) so I'm trying to find a way to treat
    those fields as such

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
     @booking = Booking.new
     @num_passengers = params[:num_passengers].to_i
@@ -7,9 +11,18 @@ class BookingsController < ApplicationController
     @num_passengers.times { @booking.passengers.build }
   end
 
-  def show
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_unit
+    end
   end
 
-  def create
-  end
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
+    end
 end
